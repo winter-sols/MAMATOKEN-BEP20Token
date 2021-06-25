392ae1d1daf2c220c430ee45f9ba41aea4a9c36e
Dong Liu <winter.solstice1219@gmail.com>
2021-06-25 23:57:27 +0300

    Update MAMAtoken.sol

--- MAMAtoken.sol
+++ MAMAtoken.sol
@@ -839,7 +839,7 @@ contract MAMATOKEN is Context, IERC20, Ownable {
         // PancakeSwap Router address:
         // (BSC testnet) 0xD99D1c33F9fC3444f8101754aBC46c52416550D1
         // (BSC mainnet) V2 0x10ED43C718714eb63d5aA57B78B54704E256024E
-        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1);
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
          // Create a uniswap pair for this new token
         uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
             .createPair(address(this), _uniswapV2Router.WETH());
